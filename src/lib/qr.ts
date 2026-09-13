8ba3ca8f1459ab1f37ee81e908da6239ef287da5
mohammedeljabu <38858265+mohammedeljabu@users.noreply.github.com>
2026-09-14 00:34:54 +0200

    Delete src directory

--- src/lib/qr.ts
+++
@@ -1,7 +0,0 @@
-import QRCode from 'qrcode'
-export async function generateQRCode(url: string) {
-  return await QRCode.toDataURL(url, { errorCorrectionLevel: 'H', margin: 2, width: 400 })
-}
-export function buildQRUrl(restaurantSlug: string, branchSlug: string, token: string) {
-  return `/r/${restaurantSlug}/b/${branchSlug}/t/${token}`
-}
--- src/lib/socket.ts
+++
@@ -1,7 +0,0 @@
-export const SocketEvents = {
-  ORDER_NEW: 'order:new',
-  ORDER_UPDATE: 'order:update',
-  ORDER_READY: 'order:ready',
-  TABLE_STATUS: 'table:status',
-  ASSISTANCE_REQUEST: 'assistance:request'
-}
